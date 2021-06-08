d60bdb4e971bb305c119b154d08f85babec053df
DESKTOP-O0BL777\x1 <joo1yeon@naver.com>
2021-06-08 12:26:27 +0900

    챗봇 UI 수정
    bottom navigation을 누르면 fragment에 챗봇 화면이 나오도록 수정

--- AndroidStudio/CareMe/app/src/main/java/com/example/careme/Chatbot.java
+++ AndroidStudio/CareMe/app/src/main/java/com/example/careme/Chatbot.java
@@ -1,7 +1,13 @@
 package com.example.careme;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,19 +15,147 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.careme.chatsample.common.data.fixtures.MessagesFixtures;
+import com.example.careme.chatsample.common.data.model.Message;
 import com.example.careme.chatsample.features.demo.def.DefaultMessagesActivity;
+import com.example.careme.chatsample.utils.AppUtils;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import me.relex.circleindicator.CircleIndicator;
 
-public class Chatbot extends Fragment {
+public class Chatbot extends Fragment implements MessagesListAdapter.SelectionListener, MessagesListAdapter.OnLoadMoreListener,
+        MessageInput.InputListener, MessageInput.AttachmentsListener, MessageInput.TypingListener{
+
     View view;
+    private static final int TOTAL_MESSAGES_COUNT = 100;
+
+    protected final String senderId = "0";
+    protected ImageLoader imageLoader;
+    protected MessagesListAdapter<Message> messagesAdapter;
+
+    private Menu menu;
+    private int selectionCount;
+    private Date lastLoadedDate;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.chatbot, container, false);
+        view = inflater.inflate(R.layout.activity_default_messages, container, false);
 
-        DefaultMessagesActivity.open(getContext());
+        imageLoader = ((imageView, url, payload) -> Picasso.get().load(url).into(imageView));
+
+        this.messagesList = view.findViewById(R.id.messagesList);
+        initAdapter();
+
+        MessageInput input = view.findViewById(R.id.input);
+        input.setInputListener(this);
+        input.setTypingListener(this);
+        input.setAttachmentsListener(this);
+
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        messagesAdapter.addToStart(MessagesFixtures.getTextMessage(), true);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        this.menu = menu;
+//        getMenuInflater().inflate(R.menu.chat_actions_menu);
+//        onSelectionChanged(0);
+//        return true;
+//    }
+
+
+    @Override
+    public void onLoadMore(int page, int totalItemsCount) {
+        Log.i("TAG", "onLoadMore: " + page + " " + totalItemsCount);
+        if (totalItemsCount < TOTAL_MESSAGES_COUNT) {
+            loadMessages();
+        }
+    }
+
+    @Override
+    public void onSelectionChanged(int count) {
+        this.selectionCount = count;
+        menu.findItem(R.id.action_delete).setVisible(count > 0);
+        menu.findItem(R.id.action_copy).setVisible(count > 10);
+    }
+
+    protected void loadMessages() {
+        // imitation of internet connection
+        new Handler().postDelayed(() -> {
+            ArrayList<Message> messages = MessagesFixtures.getMessages(lastLoadedDate);
+            lastLoadedDate = messages.get(messages.size() - 1).getCreatedAt();
+            messagesAdapter.addToEnd(messages, false);
+        }, 1000);
+    }
+
+    private MessagesListAdapter.Formatter<Message> getMessageStringFormatter() {
+        return message -> {
+            String createdAt = new SimpleDateFormat("MM d, EEE 'at' h:mm a", Locale.getDefault())
+                    .format(message.getCreatedAt());
+
+            String text = message.getText();
+            if (text == null)
+                text = "[attachment]";
+
+            return String.format(Locale.getDefault(), "%s: %s (%s)",
+                    message.getUser().getName(), text, createdAt);
+        };
+    }
+
+    public static void open(Context context) {
+        context.startActivity(new Intent(context, DefaultMessagesActivity.class));
+    }
+
+    private MessagesList messagesList;
+
+    @Override
+    public boolean onSubmit(CharSequence input) {
+        messagesAdapter.addToStart(
+                MessagesFixtures.getImageMessage(), true
+        );
+        return true;
+    }
+
+    @Override
+    public void onAddAttachments() {
+        messagesAdapter.addToStart(
+                MessagesFixtures.getImageMessage(), true
+        );
+    }
+
+    private void initAdapter() {
+        messagesAdapter = new MessagesListAdapter<>(senderId, imageLoader);
+        messagesAdapter.enableSelectionMode(this);
+        messagesAdapter.setLoadMoreListener(this);
+        messagesAdapter.registerViewClickListener(R.id.messageUserAvatar,
+                (view, message) -> AppUtils.showToast(getContext(),
+                        message.getUser().getName() + " avatar click", false));
+        this.messagesList.setAdapter(messagesAdapter);
+    }
+
+    @Override
+    public void onStartTyping() {
+        Log.v("Typing listener", getString(R.string.start_typing_status));
+    }
+
+    @Override
+    public void onStopTyping() {
+        Log.v("Typing listener", getString(R.string.stop_typing_status));
+    }
+
 }
